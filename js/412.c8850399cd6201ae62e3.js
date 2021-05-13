2a596f8a438b19947e12d824c9ec794d1d7c976c
Travis CI User <travis@example.org>
2021-05-13 15:59:45 +0000

    Merge pull request #472 from RedHatInsights/brumik-adding-inventory-id-aa-463
    Adding inventory_id to the toolbar

--- js/412.c8850399cd6201ae62e3.js
+++ js/412.c8850399cd6201ae62e3.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[412],{28412:(e,t,a)=>{"use strict";a.r(t),a.d(t,{default:()=>i});var n=a(92950),r=a.n(n),o=a(334),s=a(45237),u=a(30319),c=a(28212),l=a(83653);const i=function(){return r().createElement(s.Provider,{store:(0,u.S1)().getStore()},r().createElement(o.BrowserRouter,{basename:(0,l.Z)()},r().createElement(c.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.41492064e27a15fd3f7d.js.map
+//# sourceMappingURL=../sourcemaps/412.c355666ec0159bc9e5ef.js.map
--- js/665.3509c83398edf81a6095.js
+++ js/665.3509c83398edf81a6095.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[665],{14665:(t,n,a)=>{"use strict";function e(t,n){return(e=Object.setPrototypeOf||function(t,n){return t.__proto__=n,t})(t,n)}a.d(n,{Z:()=>e})}}]);
-//# sourceMappingURL=../sourcemaps/665.41492064e27a15fd3f7d.js.map
+//# sourceMappingURL=../sourcemaps/665.c355666ec0159bc9e5ef.js.map
