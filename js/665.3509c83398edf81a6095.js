8febf4ac7032c0d8693def80f702ed6d313d6a65
Travis CI User <travis@example.org>
2021-05-21 07:17:27 +0000

    Merge pull request #477 from RedHatInsights/brumik-update-inventory-id
    Added missing handlers for inventory id

--- js/665.3509c83398edf81a6095.js
+++ js/665.3509c83398edf81a6095.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[665],{14665:(t,n,a)=>{"use strict";function e(t,n){return(e=Object.setPrototypeOf||function(t,n){return t.__proto__=n,t})(t,n)}a.d(n,{Z:()=>e})}}]);
-//# sourceMappingURL=../sourcemaps/665.da7ae7484709ecd25857.js.map
+//# sourceMappingURL=../sourcemaps/665.e6290fee904896eb4af3.js.map
