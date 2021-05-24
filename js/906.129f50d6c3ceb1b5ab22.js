adc24cd08b5e84d48cdbcd4c7dfee904fc0a2055
Travis CI User <travis@example.org>
2021-05-24 15:15:08 +0000

    Merge pull request #478 from RedHatInsights/brumik-example-pagination
    Making pagination simpler

--- js/906.129f50d6c3ceb1b5ab22.js
+++ js/906.129f50d6c3ceb1b5ab22.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[906],{22122:(t,n,r)=>{"use strict";function e(){return(e=Object.assign||function(t){for(var n=1;n<arguments.length;n++){var r=arguments[n];for(var e in r)Object.prototype.hasOwnProperty.call(r,e)&&(t[e]=r[e])}return t}).apply(this,arguments)}r.d(n,{Z:()=>e})},19756:(t,n,r)=>{"use strict";function e(t,n){if(null==t)return{};var r,e,a={},u=Object.keys(t);for(e=0;e<u.length;e++)r=u[e],n.indexOf(r)>=0||(a[r]=t[r]);return a}r.d(n,{Z:()=>e})}}]);
-//# sourceMappingURL=../sourcemaps/906.e6290fee904896eb4af3.js.map
+//# sourceMappingURL=../sourcemaps/906.0c556ff50b0fcca171a3.js.map
