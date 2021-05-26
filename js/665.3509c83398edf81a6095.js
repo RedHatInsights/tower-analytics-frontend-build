7316a4dc4d943ff490de95d4545a68441ea8751f
Travis CI User <travis@example.org>
2021-05-26 15:42:11 +0000

    Merge pull request #473 from RedHatInsights/brumik-toolbar-restructure
    Broke up the tolbar filter item into smaller parts

--- js/665.3509c83398edf81a6095.js
+++ js/665.3509c83398edf81a6095.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[665],{14665:(t,n,a)=>{"use strict";function e(t,n){return(e=Object.setPrototypeOf||function(t,n){return t.__proto__=n,t})(t,n)}a.d(n,{Z:()=>e})}}]);
-//# sourceMappingURL=../sourcemaps/665.72e5b74fd66bbddc8561.js.map
+//# sourceMappingURL=../sourcemaps/665.b44190d30a9946baea74.js.map
