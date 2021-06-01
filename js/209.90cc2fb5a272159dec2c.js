513c1bcb058cee6928aeb7432378d7535ff73e1c
Travis CI User <travis@example.org>
2021-06-01 22:29:45 +0000

    Merge pull request #484 from RedHatInsights/fixing-broken-redirects-part1
    Fix AA-532 Re-enable redirecting from click through

--- js/209.90cc2fb5a272159dec2c.js
+++ js/209.90cc2fb5a272159dec2c.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[209],{36209:(e,t,n)=>{"use strict";n.r(t);var r=n(92950),a=n.n(r),o=n(12181),l=n.n(o),c=n(334),u=n(45237),s=n(30319),m=n(28212),i=n(83653);const E=function(){return a().createElement(u.Provider,{store:(0,s.S1)().getStore()},a().createElement(c.BrowserRouter,{basename:(0,i.Z)()},a().createElement(m.Z,null)))};l().render(a().createElement(E,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/209.b44190d30a9946baea74.js.map
+//# sourceMappingURL=../sourcemaps/209.ca674b85b6871d94aeab.js.map
--- js/906.129f50d6c3ceb1b5ab22.js
+++ js/906.129f50d6c3ceb1b5ab22.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[906],{22122:(t,n,r)=>{"use strict";function e(){return(e=Object.assign||function(t){for(var n=1;n<arguments.length;n++){var r=arguments[n];for(var e in r)Object.prototype.hasOwnProperty.call(r,e)&&(t[e]=r[e])}return t}).apply(this,arguments)}r.d(n,{Z:()=>e})},19756:(t,n,r)=>{"use strict";function e(t,n){if(null==t)return{};var r,e,a={},u=Object.keys(t);for(e=0;e<u.length;e++)r=u[e],n.indexOf(r)>=0||(a[r]=t[r]);return a}r.d(n,{Z:()=>e})}}]);
-//# sourceMappingURL=../sourcemaps/906.b44190d30a9946baea74.js.map
+//# sourceMappingURL=../sourcemaps/906.ca674b85b6871d94aeab.js.map
