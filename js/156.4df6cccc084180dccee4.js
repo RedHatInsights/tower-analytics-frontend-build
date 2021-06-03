debd9511db16f0e09afb106fda7f703630a48802
Travis CI User <travis@example.org>
2021-06-03 11:16:09 +0000

    Merge pull request #487 from RedHatInsights/brumik-fixing-toolbar-prop-types
    Fix: Toolbar props accepts number or string array as it should

--- js/156.4df6cccc084180dccee4.js
+++ js/156.4df6cccc084180dccee4.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[156],{96156:(e,a,n)=>{"use strict";function t(e,a,n){return a in e?Object.defineProperty(e,a,{value:n,enumerable:!0,configurable:!0,writable:!0}):e[a]=n,e}n.d(a,{Z:()=>t})}}]);
-//# sourceMappingURL=../sourcemaps/156.ca674b85b6871d94aeab.js.map
+//# sourceMappingURL=../sourcemaps/156.36740467a7b68ed3d960.js.map
--- js/412.a0ece62f8eac9e4f4455.js
+++ js/412.a0ece62f8eac9e4f4455.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[412],{28412:(e,t,a)=>{"use strict";a.r(t),a.d(t,{default:()=>i});var n=a(92950),r=a.n(n),o=a(334),s=a(45237),u=a(30319),c=a(28212),l=a(83653);const i=function(){return r().createElement(s.Provider,{store:(0,u.S1)().getStore()},r().createElement(o.BrowserRouter,{basename:(0,l.Z)()},r().createElement(c.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.ca674b85b6871d94aeab.js.map
+//# sourceMappingURL=../sourcemaps/412.36740467a7b68ed3d960.js.map
