4b8c7741cab9a747a7e9a438a8313c6392d39a34
Travis CI User <travis@example.org>
2021-06-09 19:50:09 +0000

    Merge pull request #488 from RedHatInsights/AA-527-delete-plan
    Added Delete button on List and Details view

--- js/906.129f50d6c3ceb1b5ab22.js
+++ js/906.129f50d6c3ceb1b5ab22.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[906],{22122:(t,n,r)=>{"use strict";function e(){return(e=Object.assign||function(t){for(var n=1;n<arguments.length;n++){var r=arguments[n];for(var e in r)Object.prototype.hasOwnProperty.call(r,e)&&(t[e]=r[e])}return t}).apply(this,arguments)}r.d(n,{Z:()=>e})},19756:(t,n,r)=>{"use strict";function e(t,n){if(null==t)return{};var r,e,a={},u=Object.keys(t);for(e=0;e<u.length;e++)r=u[e],n.indexOf(r)>=0||(a[r]=t[r]);return a}r.d(n,{Z:()=>e})}}]);
-//# sourceMappingURL=../sourcemaps/906.87db072f16ef6972c2b8.js.map
+//# sourceMappingURL=../sourcemaps/906.580da13a3c3a20cc0d4c.js.map
