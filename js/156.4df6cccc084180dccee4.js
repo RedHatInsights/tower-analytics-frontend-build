70b4c372ed37fcb81cffa17e55d6caf3f9c7e601
Travis CI User <travis@example.org>
2021-06-10 13:32:55 +0000

    Merge pull request #492 from RedHatInsights/brumik-updating-some-stuff-in-saveings-planner
    Visual updates to the statistic page

--- js/156.4df6cccc084180dccee4.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[156],{96156:(e,a,n)=>{"use strict";function t(e,a,n){return a in e?Object.defineProperty(e,a,{value:n,enumerable:!0,configurable:!0,writable:!0}):e[a]=n,e}n.d(a,{Z:()=>t})}}]);
-//# sourceMappingURL=../sourcemaps/156.580da13a3c3a20cc0d4c.js.map
