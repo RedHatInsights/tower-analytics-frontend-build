f879211425ee1f07ddfad31be6c08bc07a12732e
Travis CI User <travis@example.org>
2021-06-11 16:23:50 +0000

    Merge pull request #495 from RedHatInsights/AA-543-savings-planner-empty-view
    Added EmptyList component to show message and Add button

--- js/209.77274f702fbd776a8157.js
+++ js/209.77274f702fbd776a8157.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[209],{36209:(e,t,n)=>{"use strict";n.r(t);var r=n(92950),a=n.n(r),o=n(12181),l=n.n(o),c=n(334),u=n(45237),s=n(30319),m=n(28212),i=n(83653);const E=function(){return a().createElement(u.Provider,{store:(0,s.S1)().getStore()},a().createElement(c.BrowserRouter,{basename:(0,i.Z)()},a().createElement(m.Z,null)))};l().render(a().createElement(E,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/209.63376b68dd04f3a749b7.js.map
+//# sourceMappingURL=../sourcemaps/209.f21915c394bd9a1ce65a.js.map
