a949dfcc6decb9fa03833b7af78168f4afa720f9
Travis CI User <travis@example.org>
2021-06-14 15:56:25 +0000

    Merge pull request #496 from RedHatInsights/brumik-update-broken-chart-lib
    Update broken chart lib

--- js/412.c8850399cd6201ae62e3.js
+++ js/412.c8850399cd6201ae62e3.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[412],{28412:(e,t,a)=>{"use strict";a.r(t),a.d(t,{default:()=>i});var n=a(92950),r=a.n(n),o=a(334),s=a(45237),u=a(30319),c=a(28212),l=a(83653);const i=function(){return r().createElement(s.Provider,{store:(0,u.S1)().getStore()},r().createElement(o.BrowserRouter,{basename:(0,l.Z)()},r().createElement(c.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.f21915c394bd9a1ce65a.js.map
+//# sourceMappingURL=../sourcemaps/412.2abd7d0c56551b55d4de.js.map
