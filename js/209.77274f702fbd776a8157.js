6d60ebe51d2087e0d4d43ee2d725b53495b90c04
Travis CI User <travis@example.org>
2021-06-16 21:21:07 +0000

    Merge pull request #499 from RedHatInsights/aa-544-savingsPlanner-rbac
    AA-544 add basic RBAC for add, edit, delete Savings Plan.

--- js/209.77274f702fbd776a8157.js
+++ js/209.77274f702fbd776a8157.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[209],{36209:(e,t,n)=>{"use strict";n.r(t);var r=n(92950),a=n.n(r),o=n(12181),l=n.n(o),c=n(334),u=n(45237),s=n(30319),m=n(28212),i=n(83653);const E=function(){return a().createElement(u.Provider,{store:(0,s.S1)().getStore()},a().createElement(c.BrowserRouter,{basename:(0,i.Z)()},a().createElement(m.Z,null)))};l().render(a().createElement(E,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/209.58654d514fe70e88c628.js.map
+//# sourceMappingURL=../sourcemaps/209.1ca8def2bc9549a79b29.js.map
