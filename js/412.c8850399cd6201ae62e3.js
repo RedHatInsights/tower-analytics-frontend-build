9694447ed4f1455bdc1553ed371eb683acc8c879
Travis CI User <travis@example.org>
2021-07-01 17:25:16 +0000

    Merge pull request #515 from RedHatInsights/fixTrailingSlash
    fix trailing slash with savings planner urls

--- js/412.c8850399cd6201ae62e3.js
+++ js/412.c8850399cd6201ae62e3.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[412],{28412:(e,t,a)=>{"use strict";a.r(t),a.d(t,{default:()=>i});var n=a(92950),r=a.n(n),o=a(334),s=a(45237),u=a(30319),c=a(28212),l=a(83653);const i=function(){return r().createElement(s.Provider,{store:(0,u.S1)().getStore()},r().createElement(o.BrowserRouter,{basename:(0,l.Z)()},r().createElement(c.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.6bb2c5f0c124c9914294.js.map
+//# sourceMappingURL=../sourcemaps/412.10e3f41055576a865355.js.map
