241f6b51b28a24c8ea1043878933d09620903279
Travis CI User <travis@example.org>
2021-07-27 17:54:56 +0000

    Merge pull request #535 from RedHatInsights/job-explorer-sort
    [AA-177] Add column sorting to JobExplorer table

--- js/412.d492f547568db71e9020.js
+++ js/412.d492f547568db71e9020.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[412],{28412:(e,t,a)=>{"use strict";a.r(t),a.d(t,{default:()=>m});var n=a(92950),r=a.n(n),l=a(334),u=a(45237),c=a(30319),o=a(28212),s=a(83653),i=a(14245);const m=function(){return r().createElement(u.Provider,{store:(0,c.S1)().getStore()},r().createElement(i.Z,null,r().createElement(l.BrowserRouter,{basename:(0,s.Z)()},r().createElement(o.Z,null))))}}}]);
-//# sourceMappingURL=../sourcemaps/412.e4326026584c37d3d4f0.js.map
+//# sourceMappingURL=../sourcemaps/412.6c2e26187613ae38e92a.js.map
