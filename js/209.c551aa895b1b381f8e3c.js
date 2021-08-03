83a9af3a5157e81a23faf9ac654939b0ce071868
Travis CI User <travis@example.org>
2021-08-03 07:49:56 +0000

    Merge pull request #543 from RedHatInsights/brumik-api-checkup
    useRequest usage unification

--- js/209.c551aa895b1b381f8e3c.js
+++ js/209.c551aa895b1b381f8e3c.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[209],{36209:(e,t,n)=>{"use strict";n.r(t);var r=n(92950),a=n.n(r),l=n(12181),o=n.n(l),c=n(334),u=n(45237),s=n(30319),m=n(28212),i=n(83653),E=n(14245);const d=function(){return a().createElement(u.Provider,{store:(0,s.S1)().getStore()},a().createElement(E.Z,null,a().createElement(c.BrowserRouter,{basename:(0,i.Z)()},a().createElement(m.Z,null))))};o().render(a().createElement(d,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/209.33acea510ebae5cb646f.js.map
+//# sourceMappingURL=../sourcemaps/209.e33c683b9bfa1bd97bc0.js.map
