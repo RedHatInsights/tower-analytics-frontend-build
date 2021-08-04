fcb016b2a98c3190385324d822a2c9bd0f207ab9
Travis CI User <travis@example.org>
2021-08-04 17:32:14 +0000

    Merge pull request #545 from RedHatInsights/brumik-roi-fix
    Fix: don't show NoData while loading the ROI calculator

--- js/209.c551aa895b1b381f8e3c.js
+++ js/209.c551aa895b1b381f8e3c.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[209],{36209:(e,t,n)=>{"use strict";n.r(t);var r=n(92950),a=n.n(r),l=n(12181),o=n.n(l),c=n(334),u=n(45237),s=n(30319),m=n(28212),i=n(83653),E=n(14245);const d=function(){return a().createElement(u.Provider,{store:(0,s.S1)().getStore()},a().createElement(E.Z,null,a().createElement(c.BrowserRouter,{basename:(0,i.Z)()},a().createElement(m.Z,null))))};o().render(a().createElement(d,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/209.2b4e16f4a38bd418f742.js.map
+//# sourceMappingURL=../sourcemaps/209.f598429d78e317f40eeb.js.map
