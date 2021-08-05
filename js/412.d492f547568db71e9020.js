18afd679a2d1d11065dc5bec6c5a94984db34759
Travis CI User <travis@example.org>
2021-08-05 14:29:55 +0000

    Merge pull request #524 from RedHatInsights/brumik-chart-jsons
    First set of reports

--- js/412.d492f547568db71e9020.js
+++ js/412.d492f547568db71e9020.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[412],{28412:(e,t,a)=>{"use strict";a.r(t),a.d(t,{default:()=>m});var n=a(92950),r=a.n(n),l=a(334),u=a(45237),c=a(30319),o=a(28212),s=a(83653),i=a(14245);const m=function(){return r().createElement(u.Provider,{store:(0,c.S1)().getStore()},r().createElement(i.Z,null,r().createElement(l.BrowserRouter,{basename:(0,s.Z)()},r().createElement(o.Z,null))))}}}]);
-//# sourceMappingURL=../sourcemaps/412.35414ecf720a94c56995.js.map
+//# sourceMappingURL=../sourcemaps/412.d074456dee5e8e9bc2ec.js.map
