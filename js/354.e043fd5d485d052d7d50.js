3f609c4bd03fd9b17a985c1a898695347f094820
Travis CI User <travis@example.org>
2021-08-06 12:25:33 +0000

    Merge pull request #551 from RedHatInsights/report-chart-fix
    Change charts to line, add domainPadding to y-axis, and tooltips to all lines

--- js/354.e043fd5d485d052d7d50.js
+++ js/354.e043fd5d485d052d7d50.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[354],{91354:(n,t,a)=>{"use strict";a.d(t,{Z:()=>e});const e=function(n){return new Intl.NumberFormat("en-US",{style:"currency",currency:"USD"}).format(n)}}}]);
-//# sourceMappingURL=../sourcemaps/354.2974b887c4f43ec1e72f.js.map
+//# sourceMappingURL=../sourcemaps/354.e771ba8081d3664fce9c.js.map
