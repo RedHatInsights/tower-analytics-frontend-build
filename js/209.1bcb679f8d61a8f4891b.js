45523f3e8a9f4490ebc4e890bdb6bd80bef8eaca
Travis CI User <travis@example.org>
2021-08-10 07:44:17 +0000

    Merge pull request #553 from RedHatInsights/optimizing-breadcrumbs
    Optimized breadcrumb redirects - not reloading the page

--- js/209.1bcb679f8d61a8f4891b.js
+++ js/209.1bcb679f8d61a8f4891b.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[209],{36209:(e,t,n)=>{"use strict";n.r(t);var r=n(92950),a=n.n(r),l=n(12181),o=n.n(l),c=n(334),u=n(45237),s=n(30319),m=n(62622),i=n(83653),E=n(14245);const d=function(){return a().createElement(u.Provider,{store:(0,s.S1)().getStore()},a().createElement(E.Z,null,a().createElement(c.BrowserRouter,{basename:(0,i.Z)()},a().createElement(m.Z,null))))};o().render(a().createElement(d,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/209.12671b6ae2420a63a143.js.map
+//# sourceMappingURL=../sourcemaps/209.aef8da796858144dde3a.js.map
