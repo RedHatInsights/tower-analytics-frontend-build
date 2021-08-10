89449edf6801efc8de65dc1aa8a768945aee13ec
Travis CI User <travis@example.org>
2021-08-10 17:09:40 +0000

    Merge pull request #556 from RedHatInsights/flatten-plan-api
    Update frontend to handle flatten savings planner response

--- js/354.4c33cc5d7c665352d1bf.js
+++ js/354.4c33cc5d7c665352d1bf.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[354],{91354:(n,t,a)=>{"use strict";a.d(t,{Z:()=>e});const e=function(n){return new Intl.NumberFormat("en-US",{style:"currency",currency:"USD"}).format(n)}}}]);
-//# sourceMappingURL=../sourcemaps/354.5874bff5ffd5398f9826.js.map
+//# sourceMappingURL=../sourcemaps/354.820dec0e1c148797d3d7.js.map
