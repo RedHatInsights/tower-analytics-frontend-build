7de3ee32fae16a24539362ace669737b8e49ddef
Travis CI User <travis@example.org>
2021-08-11 08:15:59 +0000

    Merge pull request #555 from RedHatInsights/converting-toolbar-to-ts
    Converting toolbar to TS

--- js/986.194140f8a50078e7c075.js
+++ js/986.194140f8a50078e7c075.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[986],{87986:(t,e,n)=>{"use strict";n.d(e,{Z:()=>h});var r=n(22122),i=n(92950),s=n.n(i),a=n(334),o=n(45697),u=n.n(o),c=n(25914),p=function(t){var e=function(e){var n=e.id,i=e.margin,o=(0,a.useHistory)();return s().createElement(t,(0,r.Z)({},e,{getWidth:function(){return parseInt(c.Ys("#"+n).style("width"))-i.left-i.right||700},getHeight:function(){return parseInt(c.Ys("#"+n).style("height"))-i.top-i.bottom||450},history:o}))};return e.propTypes={id:u().string,margin:u().object},e};p.propTypes={Chart:u().element};const h=p}}]);
-//# sourceMappingURL=../sourcemaps/986.3c43f54614289bf3bad9.js.map
+//# sourceMappingURL=../sourcemaps/986.7ed3cd09ce8e342cc538.js.map
