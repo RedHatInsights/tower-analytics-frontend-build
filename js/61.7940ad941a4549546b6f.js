d425ef4823670e3ffe50cd82c50530522a11312d
Travis CI User <travis@example.org>
2021-08-12 12:31:54 +0000

    Merge pull request #563 from RedHatInsights/empty-reports
    Add EmptyList when no data is present for Reports

--- js/61.7940ad941a4549546b6f.js
+++ js/61.7940ad941a4549546b6f.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[61],{83061:(t,e,a)=>{"use strict";a.d(e,{Z:()=>o});var n=a(92950),c=a.n(n),i=a(58788),l=a(76473);const o=function(){return c().createElement(i.EmptyState,{variant:i.EmptyStateVariant.full},c().createElement(i.EmptyStateIcon,{icon:l.TF}),c().createElement(i.Title,{headingLevel:"h5",size:"lg"},"No Data"))}}}]);
-//# sourceMappingURL=../sourcemaps/61.035fd5f0bf8ff2166576.js.map
+//# sourceMappingURL=../sourcemaps/61.88e428735717373e6260.js.map
