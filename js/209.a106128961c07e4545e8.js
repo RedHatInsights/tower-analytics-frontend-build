7955c806a7f6f918943e839f21483f7d6c07eae1
Travis CI User <travis@example.org>
2021-08-16 08:54:16 +0000

    Merge pull request #567 from RedHatInsights/savings-planner-fix
    Update Savings Planner to handle new API outputs

--- js/209.a106128961c07e4545e8.js
+++ js/209.a106128961c07e4545e8.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[209],{36209:(e,t,n)=>{"use strict";n.r(t);var r=n(92950),a=n.n(r),l=n(12181),o=n.n(l),c=n(334),u=n(45237),s=n(30319),m=n(19406),i=n(83653),E=n(14245);const d=function(){return a().createElement(u.Provider,{store:(0,s.S1)().getStore()},a().createElement(E.Z,null,a().createElement(c.BrowserRouter,{basename:(0,i.Z)()},a().createElement(m.Z,null))))};o().render(a().createElement(d,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/209.103c8d6894c561cdaee1.js.map
+//# sourceMappingURL=../sourcemaps/209.76c2816c1c66184952b6.js.map
