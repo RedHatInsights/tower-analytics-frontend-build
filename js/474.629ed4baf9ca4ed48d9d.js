8f54fc16e09f26062929ed85a833a1cffbfe6b8b
Travis CI User <travis@example.org>
2021-08-23 16:25:23 +0000

    Merge pull request #572 from RedHatInsights/fix-no-param-in-url
    Fix: the default params were never in the URL, now they are always

--- js/474.629ed4baf9ca4ed48d9d.js
+++ js/474.629ed4baf9ca4ed48d9d.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[474],{83474:(a,t,s)=>{"use strict";s.d(t,{Z:()=>n});const n=s(45994).Z}}]);
-//# sourceMappingURL=../sourcemaps/474.7d53210600eda6fba5e9.js.map
+//# sourceMappingURL=../sourcemaps/474.b0bf50e16de477a6c002.js.map
