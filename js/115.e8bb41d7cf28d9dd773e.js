f75176763ff0091fefebafa702f19c52124055c1
Travis CI User <travis@example.org>
2021-08-25 18:23:28 +0000

    Merge pull request #575 from RedHatInsights/AA-695_FixProjectedSavings
    Fix Projected Savings to Display on Card

--- js/115.e8bb41d7cf28d9dd773e.js
+++ js/115.e8bb41d7cf28d9dd773e.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[115],{14115:(a,t,n)=>{"use strict";n.d(t,{Z:()=>c});var r=n(20806),s=n(17563);const c=function(a,t){return function(n){var c=(0,s.stringify)(n,{arrayFormat:"bracket"});a.push({pathname:r.n[t],search:c})}}}}]);
-//# sourceMappingURL=../sourcemaps/115.dfe2e3b4ad8bddec4d18.js.map
+//# sourceMappingURL=../sourcemaps/115.fa8ddab749b51f9ee850.js.map
