6cffdd2cd642ec2aa03131c4b232d77a5ffad1ed
Travis CI User <travis@example.org>
2021-09-01 15:53:42 +0000

    Merge pull request #577 from RedHatInsights/brumik-grouping-d3-together
    Grouped utilities tied to d3 charts to the charts folder

--- js/354.e043fd5d485d052d7d50.js
+++ js/354.e043fd5d485d052d7d50.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[354],{91354:(n,t,a)=>{"use strict";a.d(t,{Z:()=>e});const e=function(n){return new Intl.NumberFormat("en-US",{style:"currency",currency:"USD"}).format(n)}}}]);
-//# sourceMappingURL=../sourcemaps/354.fa8ddab749b51f9ee850.js.map
+//# sourceMappingURL=../sourcemaps/354.07033006ffdab06ef9a2.js.map
