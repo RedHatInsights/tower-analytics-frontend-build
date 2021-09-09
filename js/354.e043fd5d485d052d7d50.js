1f5d0a9837d3afc215ecec73ac7c4a88d3244baa
Travis CI User <travis@example.org>
2021-09-09 16:54:27 +0000

    Merge pull request #589 from RedHatInsights/pagination_count_fix
    Extended condition to also check if value is not 0

--- js/354.e043fd5d485d052d7d50.js
+++ js/354.e043fd5d485d052d7d50.js
@@ -1,2 +1,2 @@
 (self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[354],{91354:(n,t,a)=>{"use strict";a.d(t,{Z:()=>e});const e=function(n){return new Intl.NumberFormat("en-US",{style:"currency",currency:"USD"}).format(n)}}}]);
-//# sourceMappingURL=../sourcemaps/354.9d525f8b7f63bcd76bd6.js.map
+//# sourceMappingURL=../sourcemaps/354.b66fced499af969588b6.js.map
