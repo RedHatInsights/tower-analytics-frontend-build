4760e9b45f76cd93ded103ef8aaee20557aa8040
Travis CI User <travis@example.org>
2021-10-07 14:36:31 +0000

    Merge pull request #618 from RedHatInsights/brumik-unroll-report
    [Refactor] Update reports with TS for better dev exp.

--- js/61.d8558e419bfbd8ec172a.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[61],{83061:(t,e,a)=>{a.d(e,{Z:()=>o});var n=a(92950),c=a.n(n),i=a(58788),l=a(76473);const o=function(){return c().createElement(i.EmptyState,{variant:i.EmptyStateVariant.full},c().createElement(i.EmptyStateIcon,{icon:l.TF}),c().createElement(i.Title,{headingLevel:"h5",size:"lg"},"No Data"))}}}]);
-//# sourceMappingURL=../sourcemaps/61.cccf0424cd390c40f45756c5c2e3b11b.js.map
