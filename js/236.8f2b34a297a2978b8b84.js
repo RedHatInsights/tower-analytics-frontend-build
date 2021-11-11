d15cf65f09694f6d3ca504ae0922fb49ece5d40d
Travis CI User <travis@example.org>
2021-11-11 18:32:54 +0000

    Merge pull request #656 from RedHatInsights/brumik-roi-to-report
    [AA-847] ROI to a report

--- js/236.8f2b34a297a2978b8b84.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[236],{48236:(t,e,a)=>{a.d(e,{Z:()=>m});var n=a(92950),l=a.n(n),i=a(1085),c=a(76473);const m=function(){return l().createElement(i.EmptyState,{variant:i.EmptyStateVariant.full},l().createElement(i.EmptyStateIcon,{icon:c.TF}),l().createElement(i.Title,{headingLevel:"h5",size:"lg"},"Loading..."),l().createElement(i.EmptyStateBody,null,"Please wait."))}}}]);
-//# sourceMappingURL=../sourcemaps/236.bc5259e35ae43d4541999f65e5ca4ebd.js.map
