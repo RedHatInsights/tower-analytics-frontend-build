f5762f97187bbbb17d6842389a929c1cec750569
Travis CI User <travis@example.org>
2021-12-10 19:12:50 +0000

    Merge pull request #665 from RedHatInsights/globalNotification
    Download notifications

---
+++ js/236.1b295d1d9eebf1a92d79.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[236],{48236:(t,e,a)=>{a.d(e,{Z:()=>m});var n=a(92950),l=a.n(n),i=a(1085),c=a(76473);const m=function(){return l().createElement(i.EmptyState,{variant:i.EmptyStateVariant.full},l().createElement(i.EmptyStateIcon,{icon:c.TF}),l().createElement(i.Title,{headingLevel:"h5",size:"lg"},"Loading..."),l().createElement(i.EmptyStateBody,null,"Please wait."))}}}]);
+//# sourceMappingURL=../sourcemaps/236.9dd725f7f8767a84c069d3f138aab468.js.map
