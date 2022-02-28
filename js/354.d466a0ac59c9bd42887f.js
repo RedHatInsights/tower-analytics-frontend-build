ac86a97f3e336b9cfa4cecfca46498955b1c6146
Travis CI User <travis@example.org>
2022-02-28 18:35:25 +0000

    Merge pull request #764 from RedHatInsights/routes_fix
    AA-1046 - Updated ROI menu in left navigation

--- js/354.d466a0ac59c9bd42887f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[354],{91354:(n,t,a)=>{a.d(t,{Z:()=>e});const e=function(n){return new Intl.NumberFormat("en-US",{style:"currency",currency:"USD"}).format(n)}}}]);
