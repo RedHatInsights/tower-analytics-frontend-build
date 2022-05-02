8be9b7a335776aee4bf3e7096bceaa6f1433feb2
Travis CI User <travis@example.org>
2022-05-02 19:30:00 +0000

    Merge pull request #788 from RedHatInsights/AA1121_set_chartType_to_default
    Set chartType to default if undefined

---
+++ js/30.8236d5d007fb944560ea.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[30],{22030:(t,a,n)=>{n.d(a,{Z:()=>e});var s=n(27443);const e={get:""+s.n.reports,details:[s.n.reports+"/:slug","/automation_calculator"],getDetails:function(t){return s.n.reports+"/"+t}}}}]);
--- js/30.f6d478b246f6259371f9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[30],{22030:(t,a,n)=>{n.d(a,{Z:()=>e});var s=n(20806);const e={get:""+s.n.reports,details:[s.n.reports+"/:slug","/automation_calculator"],getDetails:function(t){return s.n.reports+"/"+t}}}}]);
