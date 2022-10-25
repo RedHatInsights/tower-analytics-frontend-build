aedf73ae72fe8a28a8d156c51e2c7b9873a0e84f
Travis CI User <travis@example.org>
2022-10-25 12:59:27 +0000

    Merge pull request #871 from fullsushidev/fix_broken_nav_tests
    Fix broken navigation tests

---
+++ js/146.1666702627908.845437b480c5dde6d902.js
@@ -0,0 +1,1 @@
+(self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[146],{90146:()=>{}}]);
---
+++ js/8361.1666702627908.845437b480c5dde6d902.js
@@ -0,0 +1,1 @@
+(self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[8361],{88361:()=>{}}]);
