93dc47531935f490650c2e0ecf91db7c1690780c
Travis CI User <travis@example.org>
2023-01-04 09:42:00 +0000

    Merge pull request #907 from RedHatInsights/zita_fix_local_navbar_loading
    Fix left nav bar in standalone mode

---
+++ js/1027.1672825190149.df74574fa0bbb1b31fc1.js
@@ -0,0 +1,1 @@
+(self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[1027],{91027:()=>{}}]);
--- js/6024.1671448284362.c8491caad4452a85317a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[6024],{56024:()=>{}}]);
