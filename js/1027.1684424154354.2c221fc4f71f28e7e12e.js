b859fcc89e96dc55f350aee59f286b93f1965af0
Travis CI User <travis@example.org>
2023-05-18 19:20:28 +0000

    Merge pull request #947 from RedHatInsights/AA-1512_templates-by-org-pdf
    [AA-1512] Templates by org pdf

--- js/1027.1684424154354.2c221fc4f71f28e7e12e.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[1027],{91027:()=>{}}]);
---
+++ js/3818.1684437407342.0acf9c772ee5ec53e5c1.js
@@ -0,0 +1,1 @@
+(self.webpackChunkautomation_analytics=self.webpackChunkautomation_analytics||[]).push([[3818],{93818:()=>{}}]);
